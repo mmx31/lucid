796e9badf0ef7be2787bcbfd43356e102cf31a3b
Will Riley <will@psychdesigns.net>
2007-04-03 19:08:17 +0000

    whoops forgot a semicolon
    git-svn-id: http://svn.psychdesktop.net/trunk@5 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -12,5 +12,5 @@ $db_password="92429242";
 $db_prefix="";
 //the secret word for encryption of passwords//NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
 
-$conf_secretword="483e74f6eb0de99a7f49ff9172acefdf"
+$conf_secretword="483e74f6eb0de99a7f49ff9172acefdf";
 ?>
