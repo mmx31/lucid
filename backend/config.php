29680370c8d59339a3601df01ad235dbd2cdd26f
Will Riley <will@psychdesigns.net>
2007-04-03 19:05:42 +0000

    Just updated the secret word instructions...
    git-svn-id: http://svn.psychdesktop.net/trunk@4 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -10,6 +10,7 @@ $db_username="root";
 $db_password="92429242";
 //the database prefix
 $db_prefix="";
-//the secret word for encryption of passwords
-$conf_secretword="blah"
+//the secret word for encryption of passwords//NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
+
+$conf_secretword="483e74f6eb0de99a7f49ff9172acefdf"
 ?>
