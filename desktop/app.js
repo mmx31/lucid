207b179047dea1e44dafa5d098b0f6a7fd253e13
Will Riley <will@psychdesigns.net>
2007-04-04 00:10:30 +0000

    Fixed a whole bunch of stuff. Got issues with dynamic menu fixed. Removed console from SQL DB b/c it worked in the old desktop and not the dojo desktop
    git-svn-id: http://svn.psychdesktop.net/trunk@13 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/app.js
+++ desktop/app.js
@@ -9,6 +9,7 @@ var app_code;
 var app_lib;
 var app_liborcode;
 var xml_seperator = "[==separator==]";
+var app_xmlHttp;
 
 function app_launch(id)
 {
@@ -26,9 +27,9 @@ exec_app(id, "code");
 }
 }
 
+//this should probably be under menu.js, but whatever...
 function app_getApplications() {
 ui_loadingIndicator(0);
-app_liborcode = libCodeProxy;
 app_createRequest();
 var url = "../backend/app.php?action=getPrograms";
 app_xmlHttp.open("GET", url, true);
@@ -37,27 +38,31 @@ app_xmlHttp.send(null);
 }
 
 function app_AppListState(){
-
 if(app_xmlHttp.readyState == 4){
 app_return = app_xmlHttp.responseText;
-
+html = '<table cellpadding="0" cellspacing="0" width="90%">';
 rawcode = app_return.split(xml_seperator);
-app_amount = rawcode[0];
+app_amount = rawcode.length;
+app_amount--;
+app_amount = app_amount/2;
 var x = 0;
-var y = 1;
-var z = 2;
+var y = 0;
+var z = 1;
 while (x <= app_amount)
    {
    var app_id = rawcode[y];
    var app_name = rawcode[z];
-    this.html += '<tr>';
-   this.html += '<td onClick = \'app_launch(' + y + ');\' style=\'border-top: 1px solid white;\'>' + z + '</td>';
-   this.html += '</tr>';
+   html += '<tr>';
+   html += '<td onClick = \'app_launch(' + app_id + ');\' style=\'border-top: 1px solid white;\'>' + app_name + '</td>';
+   html += '</tr>';
    x++;
-   y++;
-   z++;
+   y++; y++;
+   z++; z++;
    }
+html += '<tr><td onClick="logout();" style="border-top: 1px solid white; border-bottom: 1px solid white;">Logout</td></tr>';
+html += '</table>';
 
+document.getElementById("menu").innerHTML = html;
 }
 ui_loadingIndicator(1);
 }
@@ -73,31 +78,22 @@ app_xmlHttp.onreadystatechange = app_StateChange;
 app_xmlHttp.send(null);
 }
 
-var app_xmlHttp;
-function app_createRequest(){
-
+function app_createRequest()
+{
 if(window.ActiveXObject){
-
 app_xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-
 }
 else if(window.XMLHttpRequest){
-
 app_xmlHttp = new XMLHttpRequest();
-
 }
-
 }
 
 function app_StateChange(){
-
 if(app_xmlHttp.readyState == 4){
 app_return = app_xmlHttp.responseText;
-
 rawcode = app_return.split(xml_seperator);
 app_lib = rawcode[5];
 app_code = rawcode[4];
-
 if(app_liborcode == "lib")
 {
 eval(app_lib);
