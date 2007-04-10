ec6319721968198f2d56bb689b8ddcf383412e5a
Jay MacDonald <jaymac407@gmail.com>
2007-04-10 13:12:59 +0000

    git-svn-id: http://svn.psychdesktop.net/trunk@21 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -1,16 +1,4 @@
-<?php
-
-//database name
-$db_name="test";
-//database host
-$db_host="127.0.0.1";
-//database username
-$db_username="root";
-//database password
-$db_password="92429242";
-//the database prefix
-$db_prefix="";
-//the secret word for encryption of passwords//NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
+<?php
+//database type (mysql and ini) ini coming soon!$db_type="mysql";//database name$db_name="test";//database host$db_host="127.0.0.1";//database username$db_username="root";//database password$db_password="92429242";//the database prefix$db_prefix="";//the secret word for encryption of passwords//NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
 
-$conf_secretword="483e74f6eb0de99a7f49ff9172acefdf";
-?>
+$conf_secretword="483e74f6eb0de99a7f49ff9172acefdf";?>
