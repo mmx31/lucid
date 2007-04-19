f15ff5913d9197ab1d3eeef6aaea3768c95a854f
Will Riley <will@psychdesigns.net>
2007-04-19 01:45:23 +0000

    Do you like toasters with your APIs?
    git-svn-id: http://svn.psychdesktop.net/trunk@49 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/tooltip.js
+++ desktop/tooltip.js
@@ -3,3 +3,7 @@
 |   Tooltip Engine Library   |
 |   (c) 2006 Psych Designs   |
 \***************************/ 
+
+api.toaster = function(message){
+dojo.event.topic.publish("psychdesktop", message);
+}
