a72c76beab00759a1df7cdf34a6ddc95a803e339
Jay MacDonald <jaymac407@gmail.com>
2007-04-20 20:17:50 +0000

    Registry API (inside api.js) now uses Dojo to parse registry values and save registry values.
    git-svn-id: http://svn.psychdesktop.net/trunk@54 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api.js
+++ desktop/api.js
@@ -6,6 +6,7 @@
 | 	  via api.functionname();      |
 \**********************************/
 function api() {
+var api_xmlHttp;
 this.createvar = function(name)
 {
 eval("var "+name+";");
@@ -17,30 +18,51 @@ this.registry = function() { }
 this.registry = new this.registry();
 
 this.registry.getValue = function(appid,varname)  {
+/*
 ui_loadingIndicator(0);
-app_createRequest();
+//app_createRequest();
 api.registry.appid = appid;
 api.registry.varname = varname;
 var url = "../backend/api.php?registry=load&appid="+appid+"&varname="+varname;
 app_xmlHttp.open("GET", url, true);
 app_xmlHttp.onreadystatechange = api.registry.processRegistryGet;
 app_xmlHttp.send(null);
+*/
+ui_loadingIndicator(0);
+var url = "../backend/api.php?registry=load&appid="+appid+"&varname="+varname;
+dojo.io.bind({
+    url: url,
+	method: "get",
+    load: api.registry.processRegistryGet,
+    error: erroralert,
+    mimetype: "text/plain"
+});
 }
 this.registry.saveValue = function(appid,varname,value)  {
+/*
 ui_loadingIndicator(0);
-app_createRequest();
+//app_createRequest();
 api.registry.appid = appid;
 api.registry.varname = varname;
 api.registry.value = value;
 var url = "../backend/api.php?registry=save&appid="+appid+"&varname="+varname+"&value="+value;
 app_xmlHttp.open("POST", url, true);
 app_xmlHttp.send(null);
+*/
+ui_loadingIndicator(0);
+var url = "../backend/api.php?registry=save&appid="+appid+"&varname="+varname+"&value="+value;
+dojo.io.bind({
+    url: url,
+	method: "post",
+    error: erroralert,
+    mimetype: "text/plain"
+});
+ui_loadingIndicator(1);
 }
-this.registry.processRegistryGet = function() {
-if(app_xmlHttp.readyState == 4){
-value = app_xmlHttp.responseText;
+this.registry.processRegistryGet = function(type, data, evt) {
+value = data;
 api.registry.value = value;
-}
+ui_loadingIndicator(1);
 }
 }
 api = new api();
