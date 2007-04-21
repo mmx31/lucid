84476eea71aa35b7d763c76f7c6d7c21335231d5
Will Riley <will@psychdesigns.net>
2007-04-21 00:01:06 +0000

    oh man you must have been tired :D
    git-svn-id: http://svn.psychdesktop.net/trunk@56 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/api.php
+++ backend/api.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-	if (isset($_GET['registry'])) { 
+if (isset($_GET['registry'])) { 
 	if ($_GET['registry'] == ("load")) {
 	// prototype registry value loading system - jaymacdonald
 		include("config.php");
@@ -17,7 +17,7 @@ session_start();
 			echo($row['value']);	//give the result :D
 		}
 	}
-	else if ($_POST['registry'] == ("save")) {
+	elseif ($_GET['registry'] == ("save")) {
 	// prototype registry value saving system - jaymacdonald
 		include("config.php");
 		$userid = $_SESSION['userid']; // no longer a security risk
@@ -27,23 +27,24 @@ session_start();
 		$link = mysql_connect($db_hostname, $db_username, $db_password) or die('Could not connect: ' . mysql_error());
 		mysql_select_db($db_name) or die('Could not select database');
 		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-				while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
 		{
-			if($row["varname"] != ($null)) {
+			if($row["varname"] != NULL) {
 			$ID = $row["ID"];
-			$query = "REPLACE INTO `${db_prefix}registry` VALUES (${ID},${userid},${appid},${varname},${value});"
-			$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-	}
-	else {
-			$ID = $row["ID"];
-			$query = "INSERT INTO `${db_prefix}registry` VALUES (${userid},${appid},${varname},${value});"
-			$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-	}
-	}
+			$query = "REPLACE INTO `${db_prefix}registry` VALUES (${ID},${userid},${appid},${varname},${value});";
+			$result = mysql_query($query) or die("Query failed: " . mysql_error());
+			}
+			else
+			{
+				$ID = $row["ID"];
+				$query = "INSERT INTO `${db_prefix}registry` VALUES (${userid},${appid},${varname},${value});";
+				$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+			}			
+		}
 	}
 	else {
 	// not a valid registry command
 	echo("ERR"); 
 	}
-	}
+}
 ?>
