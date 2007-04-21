adaa49a22a8b68e5baefd43e21308c745cce5a2a
Jay MacDonald <jaymac407@gmail.com>
2007-04-21 11:44:15 +0000

    database.sql revision
    git-svn-id: http://svn.psychdesktop.net/trunk@62 815afad1-63b8-44ff-8d19-411ecde00fa5

--- database.sql
+++ database.sql
@@ -51,5 +51,3 @@ CREATE TABLE `#__registry` (
   `value` mediumtext NOT NULL
 ) TYPE=MyISAM CHARACTER SET `utf8` COLLATE `utf8_general_ci` AUTO_INCREMENT=1 ;
 REPLACE INTO `#__registry` VALUES (1,1,3,"Test","Yes... Test");
-REPLACE INTO `#__registry` VALUES (1,1,0,"bgimg","./wallpaper/default.gif");
-REPLACE INTO `#__registry` VALUES (1,1,0,"bgcolor","");
