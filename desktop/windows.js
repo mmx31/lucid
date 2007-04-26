284b5522970302f7d5d69fe35734838938a5e5c9
Will Riley <will@psychdesigns.net>
2007-04-26 00:35:06 +0000

    no more windows hopping on the bed! ...errr I mean overlapping the taskbar...
    git-svn-id: http://svn.psychdesktop.net/trunk@97 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/windows.js
+++ desktop/windows.js
@@ -10,11 +10,12 @@ function newWindow(wintitle, contents, winwidth, winheight)
 {
 windiv=document.createElement("div");
 windiv.style.position="absolute";
+windiv.style.top = "10%";
+windiv.style.left = "5%";
 windiv.style.width=winwidth;
 windiv.style.height=winheight;
 windiv.style.zindex="100";
 windiv.innerHTML=contents;
-document.body.appendChild(windiv);
-dojo.debug("Window div created");
-widget = dojo.widget.createWidget("FloatingPane", {hasShadow: true, resizable: true, displayCloseAction: true, title: wintitle, displayMaximizeAction: true, displayMinimizeAction: true, taskBarId: "appbar", toggle: "explode", toggleDuration: 300}, windiv);
+document.getElementById("windowcontainer").appendChild(windiv);
+widget = dojo.widget.createWidget("FloatingPane", {hasShadow: true, resizable: true, displayCloseAction: true, title: wintitle, displayMaximizeAction: true, displayMinimizeAction: true, taskBarId: "appbar", toggle: "explode", toggleDuration: 300, constrainToContainer: true}, windiv);
 }
