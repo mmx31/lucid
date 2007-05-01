c6cd997768f8e200176730010aafc5a89704fffc
Jay MacDonald <jaymac407@gmail.com>
2007-05-01 17:31:35 +0000

    fixed sql dump errors and starting a api.userLevel() function which will show the logged on users userlevel.
    git-svn-id: http://svn.psychdesktop.net/trunk@109 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -2,18 +2,18 @@
 //database type (mysql and ini) ini coming soon!
 $db_type="mysql";
 //database name
-$db_name="desktop";
+$db_name="test";
 //database host
 $db_host="127.0.0.1";
 //database username
-$db_username="mysql";
+$db_username="root";
 //database password
-$db_password="mysql";
+$db_password="92429242";
 //database prefix
-$db_prefix="dsktp_";
+$db_prefix="psych_";
 //Public registration enabled?(yes/no)
-$conf_public="no";
+$conf_public="yes";
 //the secret word for encryption of passwords
 //NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
-$conf_secretword="4396697aff20ebb9c8f0bd494d080346";
+$conf_secretword="41ae2e4baf870437694c67730099f3b2";
 ?>
