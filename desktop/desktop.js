3a05f3807ce220ae9000f67bdba9b4d0b769e971
Will Riley <will@psychdesigns.net>
2007-06-16 20:52:21 +0000

    worked on startup so it takes allmost no time to load now
    git-svn-id: http://svn.psychdesktop.net/trunk@190 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/desktop.js
+++ desktop/desktop.js
@@ -28,6 +28,53 @@ window.onbeforeunload = new function()
 }
 desktop.core = new function()
 	{
+		this.init = function()
+		{
+		    dojo.require("dojo.lfx.*");
+			dojo.require("dojo.widget.*");
+			dojo.require("dojo.widget.TaskBar");
+			dojo.require("dojo.widget.LayoutContainer");
+			dojo.require("dojo.widget.FloatingPane");
+			dojo.require("dojo.widget.ResizeHandle");
+			dojo.require("dojo.widget.DomWidget");
+			dojo.require("dojo.widget.Toaster");
+			dojo.lang.extend(dojo.widget.TaskBarItem, {
+				templateCssPath: dojo.uri.Uri("./themes/default/taskbar.css"),
+				templateCssString: ""
+			});
+			dojo.lang.extend(dojo.widget.FloatingPane, {
+				templateCssPath: dojo.uri.Uri("./themes/default/window.css"),
+				templateCssString: ""
+			});
+			desktop.taskbar.draw();
+			desktop.menu.getApplications();
+			desktop.wallpaper.loadPrefs();
+			desktop.windows.desktopResize();
+			window.onresize = desktop.windows.desktopResize;
+			document.body.onmouseup = dojo.lang.hitch(desktop.menu, desktop.menu.leftclick);
+			dojo.widget.createWidget("TaskBar", {id: "appbar", width: "100%", templateCssPath: dojo.uri.dojoUri("../themes/default/taskbar.css")}, dojo.byId("appbar"));
+			dojo.byId("appbar_container").style.border="0px";
+			dojo.byId("appbar_container").style.margin="2px";
+			dojo.byId("appbar_container").style.backgroundColor="transparent";
+			dojo.byId("appbar_container").style.padding="2px";
+			dojo.byId("appbar_container").style.width="100%";
+			dojo.byId("appbar_container").style.height="100%";
+			dojo.byId("appbar_container").style.overflow="hidden";
+			api.registry.getValue(-1,"taskbarVisibility",desktop.taskbar.setVisibility);
+			api.user.getUserName(function(data){
+				dojo.byId("menu_name").innerHTML = "<i>"+data+"</i>";
+			});
+		}
+		dojo.addOnLoad(this.init);
+		this.debug = function()
+		{
+				win = new api.window;
+				win.write("<input type='text' id='eval' /><input type='button' onClick = 'eval(dojo.byId(\"eval\").value);' />");
+				win.height="57px";
+				win.width="208px";
+				win.title="debug";
+				win.show();
+		}
 		this.logout = function()
 		{
 			window.onbeforeunload = null;
