25a12ea79a6b8b5245b231595a227b8e37889558
Will Riley <will@psychdesigns.net>
2007-06-16 21:29:58 +0000

    look out! I've got OCD!!!
    git-svn-id: http://svn.psychdesktop.net/trunk@194 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api/crosstalk.js
+++ desktop/api/crosstalk.js
@@ -2,17 +2,18 @@ api.crosstalk = new function()
 {
     this.checkForEvents = function(callback)
     {
-        var url = "../backend/api.php?crosstalk=checkForEvents";
-        dojo.io.bind({url: url,
-        load: function(type, data, http) { api.crosstalk.eventProcess(type, data, http, callback); },
-        error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/xml"
+        dojo.io.bind({
+			url: "../backend/api.php?crosstalk=checkForEvents",
+        	load: function(type, data, http) { api.crosstalk.eventProcess(type, data, http, callback); },
+        	error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/xml"
         });
     }
     this.sendEvent = function(message, appID)
     {
-	var url = "../backend/api.php?crosstalk=sendEvent&message="+message+"&sender=test&appID="+appID;
-        dojo.io.bind({url: url,
-        error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/xml"
+        dojo.io.bind({
+			url: "../backend/api.php?crosstalk=sendEvent&message="+message+"&sender=test&appID="+appID,
+        	error: function(type, error){ api.toaster("Error in AJAX call: "+error.message); },
+			mimetype: "text/xml"
         });
     }
     this.eventArray = new Array(99,99);
@@ -21,12 +22,12 @@ api.crosstalk = new function()
 		var results = data.getElementsByTagName('event');
 		for(var i = 0; i<results.length; i++){
 		api.crosstalk.eventArray["count"] = i;
-		api.crosstalk.eventArray[i] = {};
-		api.crosstalk.eventArray[i]["message"] = results[i].firstChild.nodeValue;
-		api.crosstalk.eventArray[i]["sender"] = results[i].getAttribute("sender");
-		api.crosstalk.eventArray[i]["appID"] = results[i].getAttribute("appID");
+		api.crosstalk.eventArray[i] = new Object();
+		api.crosstalk.eventArray[i].message = results[i].firstChild.nodeValue;
+		api.crosstalk.eventArray[i].sender = results[i].getAttribute("sender");
+		api.crosstalk.eventArray[i].appID = results[i].getAttribute("appID");
 		}
-        if(callback) { callback() }
+        if(callback) { callback(); }
         desktop.core.loadingIndicator(1);
 	}
 }
