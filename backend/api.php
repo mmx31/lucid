ce52075f1d6c9d41046a97ee131b565034b9bb20
Jay MacDonald <jaymac407@gmail.com>
2007-06-17 01:45:35 +0000

    added more user api stuff
    git-svn-id: http://svn.psychdesktop.net/trunk@207 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/api.php
+++ backend/api.php
@@ -80,7 +80,27 @@ echo("FAIL");
  if ($_GET['action'] == "getUserName") {
  $username = $_SESSION['username'];
 echo($username);
-}
+ }
+ if ($_GET['action'] == "getUserNameFromID") {
+ $userid = $_GET["userid"]; 
+	$query = "SELECT * FROM ${db_prefix}users WHERE ID=\"${userid}\"";
+	$link = mysql_connect($db_host, $db_username, $db_password) or die('Could not connect: ' . mysql_error());
+	mysql_select_db($db_name) or die('Could not select database');
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+		while ($row = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+		echo($_row["username"]);
+		}
+ }
+ if ($_GET['action'] == "getUserIDFromName") {
+ $username = $_GET["username"]; 
+	$query = "SELECT * FROM ${db_prefix}users WHERE username=\"${username}\"";
+	$link = mysql_connect($db_host, $db_username, $db_password) or die('Could not connect: ' . mysql_error());
+	mysql_select_db($db_name) or die('Could not select database');
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+		while ($row = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+		echo($_row["ID"]);
+		}
+ }
  if ($_GET['action'] == "getUserID") {
  $userid = $_SESSION['userid'];
  echo($userid);
--- desktop/api/user.js
+++ desktop/api/user.js
@@ -25,9 +25,41 @@ api.user = new function()
         error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/plain"
         });
 	}
+	this.userIDToUserName = function(userid, callback)
+	{
+        desktop.core.loadingIndicator(0);
+        var url = "../backend/api.php?action=getUserNameFromID&userid="+userid;
+        dojo.io.bind({
+        url: url,
+        load: function(type, data, http) { api.user.processUserIDToUserName(type, data, http, callback); },
+        error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/plain"
+        });
+	}
+	this.userNameToUserID = function(username, callback)
+	{
+        desktop.core.loadingIndicator(0);
+        var url = "../backend/api.php?action=getUserIDFromName&username="+username;
+        dojo.io.bind({
+        url: url,
+        load: function(type, data, http) { api.user.processUserNameToUserID(type, data, http, callback); },
+        error: function(type, error) { api.toaster("Error in AJAX call: "+error.message); }, mimetype: "text/plain"
+        });
+	}
 	this.processGetUserID = function(type, data, evt, callback)
     {
         api.user.userID = data;
+        if(callback) { callback(data); }
+        desktop.core.loadingIndicator(1);
+    }
+	this.processUserIDToUserName = function(type, data, evt, callback)
+    {
+        api.user.username = data;
+        if(callback) { callback(data); }
+        desktop.core.loadingIndicator(1);
+    }
+	this.processUserNameToUserID = function(type, data, evt, callback)
+    {
+        api.user.username = data;
         if(callback) { callback(data); }
         desktop.core.loadingIndicator(1);
     }
