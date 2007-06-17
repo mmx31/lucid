e48da472a734b97fdafe65cd54b9fe04e9fafd9e
Jay MacDonald <jaymac407@gmail.com>
2007-06-17 22:34:50 +0000

    filesystem scraped, needs a TOTAL rethink
    git-svn-id: http://svn.psychdesktop.net/trunk@223 815afad1-63b8-44ff-8d19-411ecde00fa5

--- database.sql
+++ database.sql
@@ -85,17 +85,6 @@ CREATE TABLE `#__registry` (
   `value` mediumtext NOT NULL
 ) TYPE=MyISAM CHARACTER SET `utf8` COLLATE `utf8_general_ci` AUTO_INCREMENT=1 ;
 
--- filesystem
-DROP TABLE IF EXISTS `#__filesystem`;
-CREATE TABLE `#__filesystem` (
-  `ID` int(11) NOT NULL auto_increment PRIMARY KEY,
-  `userid` int(11) NOT NULL,
-  `path` mediumtext NOT NULL,
-  `location` mediumtext NOT NULL,
-  `sharing` varchar(100) default 'none'
-) TYPE=MyISAM CHARACTER SET `utf8` COLLATE `utf8_general_ci` AUTO_INCREMENT=1 ;
-REPLACE INTO `#__filesystem` VALUES (1,1,"docs","isDir","all");
-REPLACE INTO `#__filesystem` VALUES (1,1,"docs/ReadMe.txt","readme.txt","all");
 --crosstalk
 DROP TABLE IF EXISTS `#__crosstalk`;
 CREATE TABLE `#__crosstalk` (
