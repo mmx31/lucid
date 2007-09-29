41a7390c2278c02e7b3d90196b3fed4b7b12330c
Jay MacDonald <jaymac407@gmail.com>
2007-09-29 00:58:37 +0000

    git-svn-id: http://svn.psychdesktop.net/trunk@290 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -2,18 +2,18 @@
 //database type (mysql and ini) ini coming soon!
 $db_type="mysql";
 //database name
-$db_name="desktop";
+$db_name="test";
 //database host
 $db_host="127.0.0.1";
 //database username
-$db_username="mysql";
+$db_username="root";
 //database password
-$db_password="mysql";
+$db_password="";
 //database prefix
 $db_prefix="dsktp_";
 //Public registration enabled?(yes/no)
 $conf_public="yes";
 //the secret word for encryption of passwords
 //NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
-$conf_secretword="92bcedf6dc19357d751ef8e5c3c9ea28";
+$conf_secretword="6c4aa4866c3e081d3bcb31f282ceb6b2";
 ?>
