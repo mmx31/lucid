41ec53689da5d369c76b08ec02e8ea975a1f8ea5
Jay MacDonald <jaymac407@gmail.com>
2007-10-22 11:17:06 +0000

    sorry, I overwrote the config file; fixed
    git-svn-id: http://svn.psychdesktop.net/trunk@383 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/config.php
+++ backend/config.php
@@ -2,18 +2,18 @@
 //database type (mysql and ini) ini coming soon!
 $db_type="mysql";
 //database name
-$db_name="test";
+$db_name="desktop";
 //database host
 $db_host="127.0.0.1";
 //database username
-$db_username="root";
+$db_username="mysql";
 //database password
-$db_password="";
+$db_password="mysql";
 //database prefix
-$db_prefix="";
+$db_prefix="dsktp_";
 //Public registration enabled?(yes/no)
 $conf_public="yes";
 //the secret word for encryption of passwords
 //NOTE: DO NOT CHANGE AFTER INSTALL! THIS WILL BREAK THE USER LOGIN PROCESS!!!
-$conf_secretword="87e6e5511f614db6a33467e8202710db";
+$conf_secretword="6c4aa4866c3e081d3bcb31f282ceb6b2";
 ?>
