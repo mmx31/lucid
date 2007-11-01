0f7daa449bf52f9b6223f597f002aa2845f74871
Will Riley <will@psychdesigns.net>
2007-11-01 17:51:15 +0000

    git-svn-id: http://svn.psychdesktop.net/trunk@437 815afad1-63b8-44ff-8d19-411ecde00fa5

--- admin/backends/saveapp.php
+++ admin/backends/saveapp.php
@@ -6,16 +6,28 @@ echo "<script type='text/javascript'>window.location='index2.php?backend=app&opm
 exit();
 }
 
-$link = mysql_connect($db_host, $db_username, $db_password)   or die('Could not connect: ' . mysql_error());mysql_select_db($db_name) or die('Could not select database');
-$_POST['name'] = mysql_real_escape_string (str_replace("[==separator==]", " ", $_POST['name']), $link);$_POST['author'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['author']), $link);$_POST['email'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['email']), $link);$_POST['code'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['code']), $link);$_POST['version'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['version']), $link);$_POST['maturity'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['maturity']), $link);$_POST['category'] = mysql_real_escape_string (str_replace("[==separator==]", " ",$_POST['category']), $link);
+$link = mysql_connect($db_host, $db_username, $db_password)
+   or die('Could not connect: ' . mysql_error());
+mysql_select_db($db_name) or die('Could not select database');
+$_POST['name'] = mysql_real_escape_string ($_POST['name'], $link);
+$_POST['author'] = mysql_real_escape_string ($_POST['author'], $link);
+$_POST['email'] = mysql_real_escape_string ($_POST['email'], $link);
+$_POST['code'] = mysql_real_escape_string ($_POST['code'], $link);
+$_POST['version'] = mysql_real_escape_string ($_POST['version'], $link);
+$_POST['maturity'] = mysql_real_escape_string ($_POST['maturity'], $link);
+$_POST['category'] = mysql_real_escape_string ($_POST['category'], $link);
 if($_POST['appid'] == -1)
 {
-//INSERT$query = "INSERT INTO ${db_prefix}apps (name, author, email, code, version, maturity, category) VALUES(\"${_POST['name']}\", \"${_POST['author']}\", \"${_POST['email']}\", \"${_POST['code']}\", \"${_POST['version']}\", \"${_POST['maturity']}\", \"${_POST['category']}\")";}
+//INSERT
+$query = "INSERT INTO ${db_prefix}apps (name, author, email, code, version, maturity, category) VALUES(\"${_POST['name']}\", \"${_POST['author']}\", \"${_POST['email']}\", \"${_POST['code']}\", \"${_POST['version']}\", \"${_POST['maturity']}\", \"${_POST['category']}\")";
+}
 else
 {
 $query = "UPDATE ${db_prefix}apps  SET name=\"${_POST['name']}\", author=\"${_POST['author']}\", email=\"${_POST['email']}\", code=\"${_POST['code']}\", version=\"${_POST['version']}\", maturity=\"${_POST['maturity']}\", category=\"${_POST['category']}\" WHERE ID=\"${_POST['appid']}\" LIMIT 1";
 }
-mysql_query($query) or die('Query failed: ' . mysql_error());mysql_close($link);
+mysql_query($query) or die('Query failed: ' . mysql_error());
+mysql_close($link);
 if($_POST['action'] == "save") echo "<script type='text/javascript'>window.location=\"./index2.php?backend=app&opmessage=App+Saved+Successfully\"</script>";
 elseif($_POST['action'] == "edit") echo "<form id='form' action='./index2.php?backend=editapp#bottom' method='POST'><input type='hidden' name='appid' value='" . $_POST['appid'] . "' /></form><script type='text/javascript'>document.getElementById('form').submit();</script>";
-?>
+
+?>
