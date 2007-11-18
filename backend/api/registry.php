3092dee5ad716f00ab92be45eb0d7eec0b1cf5e7
Will Riley <will@psychdesigns.net>
2007-11-18 20:53:24 +0000

    bugfixes and such. Now the registry API seems to work.
    git-svn-id: http://svn.psychdesktop.net/trunk@499 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/api/registry.php
+++ backend/api/registry.php
@@ -1,12 +1,14 @@
 <?php
+	session_start();
 	require("../models/registry.php");
+	require("../models/user.php");
     if($_GET['section'] == "stream")
 	{
 		if($_GET['action'] == "save")
 		{
 			$p = $User->get_current();
-			$result = $Registry->filter(["userid", "appid", "name"], [$p->id, $_POST['appid'], $_POST['name']]);
-			if(!isset($result[0])) { $u = new $Registry(); $u->userid = $p->id; }
+			$result = $Registry->filter(array("userid", "appid", "name"), array($p->id, $_POST['appid'], $_POST['name']));
+			if(!isset($result[0])) { $u = new $Registry(); $u->userid = $p->id; $u->name=$_POST['name']; $u->appid = $_POST['appid']; }
 			else { $u = $result[0]; }
 			$u->value = stripslashes($_POST['value']);
 			$u->save();
@@ -15,7 +17,7 @@
 		if($_GET['action'] == "load")
 		{
 			$p = $User->get_current();
-			$result = $Config->filter(["userid", "appid", "name"], [$p->id, $_GET['appid'], $_GET['name']]);
+			$result = $Registry->filter(array("userid", "appid", "name"), array($p->id, $_GET['appid'], $_GET['name']));
 			$result = $result[0];
 			echo $result->value;
 		}
