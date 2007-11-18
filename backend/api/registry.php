d7e6a7e56d2459fdaaab69f7699ace7c23b0bdb0
Will Riley <will@psychdesigns.net>
2007-11-18 19:43:07 +0000

    worked on the registry, uses dojo.data
    git-svn-id: http://svn.psychdesktop.net/trunk@495 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/api/registry.php
+++ backend/api/registry.php
@@ -6,6 +6,7 @@
 		{
 			$p = $User->get_current();
 			$result = $Registry->filter("userid", $p->id); //TODO: here's where we need to be able to chain filters somehow.
+			//select using $_POST['appid'] and $_POST['name']
 			if(!isset($result[0])) { $u = new $Registry(); $u->userid = $p->id; }
 			else { $u = $result[0]; }
 			$u->value = stripslashes($_POST['value']);
@@ -16,6 +17,7 @@
 		{
 			$p = $User->get_current();
 			$result = $Config->filter("userid", $p->id); //TODO: here's where we need to be able to chain filters somehow.
+			//The app id will be $_GET['appid'] and the name will be $_GET['name']
 			$result = $result[0];
 			echo $result->value;
 		}
--- desktop/api/registry.js
+++ desktop/api/registry.js
@@ -1,3 +1,5 @@
+dojo.require("dojo.data.ItemFileWriteStore");
+
 /*
  * Group: api
  * 
@@ -7,8 +9,30 @@
  * 		An API that allows storage in a table format for users.
  * 
  */
-api.registry = function()
+//desktop.core.backend("api.registry.stream.save")
+api.registry = function(name, appID)
 {
-	//desktop.core.backend("api.registry.stream.save")
+	return dojo.mixin(new dojo.data.ItemFileWriteStore({
+		url: desktop.core.backend("api.registry.stream.load")+"&appid="+encodeURIComponent(appID)+"&name="+encodeURIComponent(name)
+	}), {
+		__desktop_name: name,
+		__desktop_appid: appID,
+		_saveEverything: function(saveCompleteCallback, saveFailedCallback, newFileContentString) {
+			dojo.xhrPost({
+				url: desktop.core.backend("api.registry.stream.save"),
+				content: {
+					value: newFileContentString,
+					appid: this.__desktop_appid,
+					name: this.__desktop_name
+				},
+				load: function(data, ioArgs) {
+					saveCompleteCallback();
+				},
+				error: function(type, error) {
+					saveFailedCallback();
+				}
+			});
+		}
+	});
 }
 api.reg = api.registry;
