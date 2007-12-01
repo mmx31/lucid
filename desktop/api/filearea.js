b9a4b0bc42ca5042214953f3e119cd0cef5e110b
Will Riley <will@psychdesigns.net>
2007-12-01 21:09:06 +0000

    some work on the filearea
    git-svn-id: http://svn.psychdesktop.net/trunk@561 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api/filearea.js
+++ desktop/api/filearea.js
@@ -15,5 +15,19 @@ dojo.declare(
 	[dijit._Widget, dijit._Templated, dijit._Container, dijit._Contained],
 {
 	path: "/",
-	iconStyle: "list"
+	iconStyle: "list",
+	templateString: "<div class='desktopFileArea' dojoAttachPoint='focusNode,containerNode' style='overflow-x: hidden; overflow-y: scroll;'></div>",
+});
+
+dojo.declare(
+	"api.filearea._item",
+	[dijit._Widget, dijit._Templated, dijit._Contained],
+{
+	iconClass: "",
+	label: "file",
+	templateString: "<div class='desktopFileItem' dojoAttachPoint='focusNode'><div class='desktopFileItemIcon ${iconClass}' dojoAttachEvent='ondijitclick:_onIconClick'></div><div class='desktopFileItemText' dojoAttachEvent='ondijitclick:_onTextClick' style='text-align: center;'>${label}</div></div>",
+	onClick: function(e)
+	{
+		
+	}
 });
