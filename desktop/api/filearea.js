e7da49fa30df93990eaa713f4771f12dcf998b97
Will Riley <will@psychdesigns.net>
2007-12-01 18:44:44 +0000

    filearea isn't a layout widget (whoops)
    git-svn-id: http://svn.psychdesktop.net/trunk@558 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api/filearea.js
+++ desktop/api/filearea.js
@@ -6,11 +6,13 @@
  * 
  */
 dojo.provide("api.filearea");
-dojo.require("dijit.layout._LayoutWidget");
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require("dijit._Container");;
 
 dojo.declare(
 	"api.filearea",
-	dijit.layout._LayoutWidget,
+	[dijit._Widget, dijit._Templated, dijit._Container, dijit._Contained],
 {
 	path: "/",
 	iconStyle: "list"
