b1ec51e6dd507931a82fe7fcc22f8f2c263ef835
Jay MacDonald <jaymac407@gmail.com>
2007-12-02 14:36:22 +0000

    uploader works now
    git-svn-id: http://svn.psychdesktop.net/trunk@573 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/upload.php
+++ backend/upload.php
@@ -22,32 +22,20 @@
 session_start();
 	require("config.php");
 if(!isset($_SESSION['userid'])) {
-die("Access denied. You must login first.");
+die("Access denied. Your session has expired.");
 }
 if(isset($_FILES['uploadedfile']['name'])) {
-$target_path = 'files/'.$_SESSION['userid'].'1';
-
+$target_path = '../files/'.$_SESSION['username'].'/'.$_POST['uploadedir'];
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
-
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-   $link = mysql_connect($db_host, $db_username, $db_password) or die('Could not connect: ' . mysql_error());
-   mysql_select_db($db_name) or die('Could not select database');
-	$uid = $_SESSION['userid'];
-	$name = $_FILES['uploadedfile']['name'];
-	$dir = $_POST['uploadedir'];
-	$location = $uid.'1'.$name;
-	$query = "INSERT INTO `${db_prefix}filesystem` (userid, file, directory, location, sharing) VALUES('${uid}', '${name}', '${dir}', '${location}', 'none');";
-	mysql_select_db($db_name) or die('Could not select database');
-	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-	echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
-    " has been uploaded to your Psych Desktop :)<br> Upload another file? <p>";
+	echo "The file '".  basename( $_FILES['uploadedfile']['name']). 
+    "' has been uploaded to your Psych Desktop :)<br> Upload another file? <p>";
 } else{
     echo "There was an error uploading the file, please try again! Remember: there are size limitations.";
 }
 }
 ?>
 <form enctype="multipart/form-data" action="upload.php" method="POST">
-<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
 Choose a file to upload: <input name="uploadedfile" type="file" /><br />
 Path to place the file in: <input name="uploadedir" type="text" value="/" /><br />
 <input type="submit" value="Upload File to Psych Desktop" />
