3217357bd5bf347c2d4a5559a3f5c8c9b067c7cd
Will Riley <will@psychdesigns.net>
2007-12-03 02:29:45 +0000

    minor change
    git-svn-id: http://svn.psychdesktop.net/trunk@594 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/api/fs.php
+++ backend/api/fs.php
@@ -63,7 +63,7 @@ if($_GET['section'] == "io")
 			die("Access denied. Your session has expired.");
 		}
 		if(isset($_FILES['uploadedfile']['name'])) {
-			$target_path = '../../files/'.$_SESSION['username'].'/'.($_POST['uploadedir'] || "");
+			$target_path = '../../files/'.$_SESSION['username'].'/'.$_GET['path'];
 			$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
 			if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 			    echo "<textarea>{status: 'success', details: '" . $_FILES['uploadedfile']['name'] . "'}</textarea>";
