5ac1719263f268cae5e9764d33b41dc61d69fd63
Will Riley <will@psychdesigns.net>
2007-12-09 02:45:33 +0000

    more improvements
    git-svn-id: http://svn.psychdesktop.net/trunk@647 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/install.js
+++ install/install.js
@@ -2,45 +2,72 @@ dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.layout.StackContainer");
 dojo.require("dijit.form.Button");
 dojo.require("dijit.form.CheckBox");
+dojo.require("dijit.form.TextBox");
 dojo.require("dijit.form.Form");
 install = new function() {
-this.selected = function(page){
-	dijit.byId("previous").setDisabled(page.isFirstChild);
-	dijit.byId("next").setDisabled(page.isLastChild);
-	install.currentPage = page;
-}
-this.onLoad = function() {
-	dojo.subscribe("wizard-selectChild", install.selected);
-	install.selected(dijit.byId("start"));
-	dijit.byId("next").setDisabled(install.currentPage);
-	install.getPerms();
-	setInterval(install.getPerms, 5000)
-}
-
-this.getPerms = function()
-{
-	dojo.xhrGet({
-		url: "./backend.php?action=checkpermissions",
-		load: function(data, args)
+	this.selected = function(page){
+		dijit.byId("previous").setDisabled(page.isFirstChild);
+		dijit.byId("next").setDisabled(page.isLastChild);
+		install.currentPage = page;
+		if(page.title == "Start")
 		{
-			var html = "<ul>";
-			var ready=true;
-			for (key in data) {
-				html += "<li>" + key.replace("../", "") + ": ";
-				if (data[key] == "ok") 
-					html += "<span style='color: green'>";
-				else {
-					html += "<span style='color: red'>";
-					ready = false;
+			dijit.byId("next").setDisabled(true);
+		}
+		if(page.title == "installation type")
+		{
+			dijit.byId("next").setDisabled(true);
+		}
+	}
+	this.onLoad = function() {
+		dojo.subscribe("wizard-selectChild", install.selected);
+		install.selected(dijit.byId("start"));
+		dijit.byId("next").setDisabled(install.currentPage);
+		install.getPerms();
+		setInterval(install.getPerms, 2000);
+		setInterval(install.checkDbInput, 1000);
+		dojo.forEach(['installtype-new', 'installtype-cms', 'installtype-reset'], function(e)
+		{
+			dijit.byId(e)._clicked = install.onTypeRadioClick;
+		});
+	}
+	this.checkDbInput = function() {
+		if (install.currentPage.title == "Database") {
+			var form = dijit.byId("form");
+			
+		}
+	}
+	this.getPerms = function()
+	{
+		dojo.xhrGet({
+			url: "./backend.php?action=checkpermissions",
+			load: function(data, args)
+			{
+				var html = "<ul>";
+				var ready=true;
+				for (key in data) {
+					html += "<li>" + key.replace("../", "") + ": ";
+					if (data[key] == "ok") 
+						html += "<span style='color: green'>";
+					else {
+						html += "<span style='color: red'>";
+						ready = false;
+					}
+					html += data[key] + "</span></li>";
 				}
-				html += data[key] + "</span></li>";
-			}
-			html += "</ul>";
-			dojo.byId("perms").innerHTML = html;
-			dijit.byId("next").setDisabled(!ready);
-		},
-		handleAs: "json"
-	});
-}
+				html += "</ul>";
+				dojo.byId("perms").innerHTML = html;
+				if (install.currentPage.title == "Start") {
+					dijit.byId("next").setDisabled(!ready);
+				}
+			},
+			handleAs: "json"
+		});
+	},
+	this.onTypeRadioClick = function(e) {
+		if(!this.checked) {
+			this.setChecked(true);
+			dijit.byId("next").setDisabled(false);
+		}
+	}
 }
 dojo.addOnLoad(install.onLoad);
