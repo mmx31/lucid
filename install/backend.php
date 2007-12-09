b115909e29b9cda0aefeacc26d8ad17bc788b83a
Jay MacDonald <jaymac407@gmail.com>
2007-12-09 02:37:10 +0000

    application installer backend done
    git-svn-id: http://svn.psychdesktop.net/trunk@644 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/backend.php
+++ install/backend.php
@@ -1,5 +1,52 @@
 <?php
     $act = $_GET['action'];
+	if($act == "installprograms")
+	{
+		require("../backend/config.php");
+		mysql_connect($db_host, $db_username, $db_password) or die('<span style="color: red;">Error connecting to MySQL server: ' . mysql_error() . '</span></div></center></body></html>');
+		mysql_select_db($db_name) or die('<span style="color: red;">Error selecting MySQL database: ' . mysql_error() . '</span></div></center></body></html>');
+		mysql_query("TRUNCATE TABLE `${db_prefix}apps`;");
+		mysql_query("ALTER TABLE `${db_prefix}apps` AUTO_INCREMENT = 1;");
+		require("../backend/lib.xml.php");
+		$xml = new Xml; 
+		$dir = opendir("./apps/");
+		while(($file = readdir($dir)) !== false){
+			if($file == '..' || $file == '.'){
+				continue;
+			}
+			else {
+			if(is_dir("../apps/" . $file)){
+				if(!file_exists("../apps/" . $file . "/appmeta.xml")) { continue; }
+				$out = $xml->parse('../apps/'.$file.'/appmeta.xml', 'FILE');
+				$name = $out[name];
+				$author = $out[author];
+				$email = $out[email];
+				$version = $out[version];
+				$maturity = $out[maturity];
+				$category = $out[category];
+				$installfile = $out[installFile];
+				$message = $out[installMessage];
+				$message2 = $out[installedMessage];
+				/* no optional files required for the core apps
+				$fr = $out[filesRequired];
+				$frt = $out[filesCopyTo];
+				$frf = $out[filesCopyFrom]; */
+				echo($message);
+				$templine = '';
+				$file2 = fopen("../apps/$file/$installfile", "r");
+				while(!feof($file2)) {
+					$templine = $templine . fgets($file2, 4096);
+				}
+				fclose ($file2); 
+				$templine = mysql_real_escape_string($templine);
+				$code = $templine;
+				$blah = "INSERT INTO `${db_prefix}apps` (`name`, `author`, `email`, `code`, `version`, `maturity`, `category`) VALUES ('${name}', '${author}', '${email}', '${code}', '${version}', '${maturity}', '${category}');"; 
+				mysql_query($blah) or die("fail - ". mysql_error());
+				}
+			}
+		}
+	}
+			
 	if($act == "checkpermissions")
 	{
 		$dirs = array(
