f670f37dbd65ff65741dff30bf63ef9882293656
Will Riley <will@psychdesigns.net>
2007-12-12 23:19:14 +0000

    small fix
    git-svn-id: http://svn.psychdesktop.net/trunk@685 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/backend.php
+++ install/backend.php
@@ -1,7 +1,7 @@
 <?php
 	require("../backend/configuration.php");
 	//error_reporting(0);
-    $act = ($_GET['action'] == "" ? $_GET['action'] : "installprograms");
+    $act = ($_GET['action'] == "" ? "installprograms" : $_GET['action']);
 	if($act == "installadmin")
 	{
 		require("../backend/models/user.php");
