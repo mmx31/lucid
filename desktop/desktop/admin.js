cb8ec5ad321c3e033b6005af42f27bad13a9c970
Will Riley <will@psychdesigns.net>
2007-12-13 13:38:48 +0000

    added delete method
    git-svn-id: http://svn.psychdesktop.net/trunk@693 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/desktop/admin.js
+++ desktop/desktop/admin.js
@@ -4,8 +4,23 @@ desktop.admin = new function()
 		list: function(callback) {
 			dojo.xhrGet({
 				url: desktop.core.backend("core.administration.users.list"),
+				dsktp_callback: callback,
 				load: function(data, ioArgs) {
-					callback(dojo.fromJson(data));
+					ioArgs.args.dsktp_callback(dojo.fromJson(data));
+				}
+			});
+		},
+		delete: function(id, callback)
+		{
+			dojo.xhrPost({
+				url: desktop.core.backend("core.administration.users.delete",
+				content: {
+					id: id
+				},
+				dsktp_callback: callback,
+				load: function(data, ioArgs)
+				{
+					ioArgs.args.dsktp_callback(data == "0");
 				}
 			});
 		}
