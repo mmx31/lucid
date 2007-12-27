788955af4411d5ae28cbd7625df7b041d882d2d8
Will Riley <will@psychdesigns.net>
2007-12-27 02:16:44 +0000

    some more fixes
    git-svn-id: http://svn.psychdesktop.net/trunk@772 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/lib/includes.php
+++ backend/lib/includes.php
@@ -1,7 +1,7 @@
 <?php
 	$p = substr(__FILE__, strrpos(__FILE__,"backend"));
-	$ml = explode("/", $p);
-	$w = explode("\\", $p);
+	$ml = explode(DIRECTORY_SEPARATOR, $p);
+
 	$path = "./";
 	$l = count((isset($ml[0]) ? $ml : $w));
 	for($i=0; $i < $l-2; $i++)
@@ -10,7 +10,6 @@
 	}
 	foreach(array(
 		"configuration.php",
-		//"lib/PEAR.php", //uncomment if you don't have PEAR installed
 		"lib/MDB2.php",
 		"lib/util.php",
 		"lib/output.php",
