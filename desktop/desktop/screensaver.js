4a6f8d526a865b47daf7d863b5b10d577ed79c54
Will Riley <will@psychdesigns.net>
2007-12-29 06:33:31 +0000

    screensaver, cuz I was bored :D
    git-svn-id: http://svn.psychdesktop.net/trunk@785 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/desktop/screensaver.js
+++ desktop/desktop/screensaver.js
@@ -7,5 +7,42 @@
 */
 desktop.screensaver = new function()
 {
-	//we probably will do something similar to apps for this one...
+	this.box = document.createElement("div");
+	dojo.forEach([
+		{n: "backgroundColor", v: "black"},
+		{n: "opacity", v: 0},
+		{n: "width", v: "100%"},
+		{n: "height", v: "100%"},
+		{n: "position", v: "absolute"},
+		{n: "top", v: "0px"},
+		{n: "left", v: "0px"},
+		{n: "zIndex", v: 9999*9999}
+	], dojo.hitch(this, function(e) {
+		dojo.style(this.box, e.n, e.v);
+	}));
+	this.timeout = setTimeout(dojo.hitch(this, this.run), 1000*60*5);
+	this.run = function() {
+		clearTimeout(this.timeout);
+		document.body.appendChild(this.box);
+		var anim = dojo.fadeIn({
+			node: this.box,
+			duration: 200
+		});
+		dojo.connect(anim, "onEnd", this, function() {
+			this.moveEvent = dojo.connect(document.body, "onmousemove", this, this.stop);
+		});
+		anim.play();
+	}
+	this.stop = function() {
+		dojo.disconnect(this.moveEvent);
+		var anim = dojo.fadeOut({
+			node: this.box,
+			duration: 200
+		});
+		dojo.connect(anim, "onEnd", this, function() {
+			document.body.removeChild(this.box);
+			this.timeout = setTimeout(dojo.hitch(this, this.run), 1000*60*5);
+		});
+		anim.play();
+	}
 }
