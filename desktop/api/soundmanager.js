cce089a6759a7c5def9325ed5e51c66c437cc81d
Will Riley <will@psychdesigns.net>
2008-01-02 21:18:16 +0000

    more work on sound API
    git-svn-id: http://svn.psychdesktop.net/trunk@836 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api/soundmanager.js
+++ desktop/api/soundmanager.js
@@ -12,25 +12,49 @@ dojo.declare("api.sound", dijit._Widget, {
 	src: "",
 	loop: false,
 	timer: null,
-	time: 0,
+	position: 0,
 	startTime: 0,
 	timeInterval: 1000,
 	postCreate: function() {
 		var requiredVersion = new com.deconcept.PlayerVersion([8,0,0]);
 		var installedVersion = com.deconcept.FlashObjectUtil.getPlayerVersion();
 		this.flash=installedVersion.versionIsValid(requiredVersion);
+		if(this.flash) {
+			var aflax = new api.aflax();
+			this.flSound = new api.aflax.FlashObject(aflax, this.id+"_Flash");
+			this.flSound.exposeFunction("loadSound", this.flSound);		
+			this.flSound.exposeFunction("start", this.flSound);		
+			this.flSound.exposeFunction("stop", this.flSound);		
+			this.flSound.exposeFunction("setVolume", this);		
+			this.flSound.exposeProperty("position", this);
+		
+			this.flSound.mapFunction("addEventHandler");		
+		
+			this.flSound.addEventHandler("onLoad", dojo.hitch(this, this.ready));
+		
+			this.flSound.loadSound(this.src, true);
+		}
 	},
+	ready: function() {
+		this.flReady = true;
+	},
+	_startPos: 0,
+	flReady: false,
 	play: function() {
 		if (!this.flash) {
 			if (this.domNode.innerHTML != "") 
 				this.stop();
-			this.time = 0;
+			this.position = 0;
 			this.domNode.innerHTML = "<embed src=\"" + this.src + "\" hidden=\"true\" autoplay=\"true\" loop=\"" + (this.loop ? "true" : "false") + "\">";
 			api.soundmanager.container.appendChild(this.domNode);
 			this.timer = setInterval(dojo.hitch(this, this.fixtime), this.timeInterval);
 		}
 		else {
-			//aflax
+			if(this.flReady) {
+				this.flSound.start(this._startPos);
+				this._startPos = 0;
+			}
+			else setTimeout(dojo.hitch(this, this.play), 100);
 		}
 	},
 	pause: function() {
@@ -38,11 +62,12 @@ dojo.declare("api.sound", dijit._Widget, {
 			this.stop();
 		}
 		else {
-			//aflax
+			this._startPos = this.flSound.position;
+			this.flSound.stop();
 		}
 	},
 	fixtime: function() {
-		this.time += this.timeInterval;
+		this.position += this.timeInterval;
 	},
 	stop: function() {
 		if (!this.flash) {
@@ -50,11 +75,13 @@ dojo.declare("api.sound", dijit._Widget, {
 			this.domNode.innerHTML = "";
 		}
 		else {
-			//aflax
+			this._startPos = 0;
+			this.flSound.stop();
 		}
 	},
 	uninitialize: function() {
 		api.soundmanager.container.removeChild(this.domNode);
+		if(!this.flash) clearInterval(this.timer);
 	}
 });
 
