3182faa22e9d0c28dcd8665860e53f17d83297db
Will Riley <will@psychdesigns.net>
2008-01-16 21:42:28 +0000

    worked on the mail client a tad
    git-svn-id: http://svn.psychdesktop.net/trunk@1007 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/apps/Email Client/app.js
+++ install/apps/Email Client/app.js
@@ -1,17 +1,20 @@
 this.init = function(args) {
 	dojo.require("dijit.layout.LayoutContainer");
 	dojo.require("dijit.layout.SplitContainer");
+	dojo.require("dijit.layout.ContentPane");
 	dojo.require("dijit.Tree");
 	dojo.require("dijit.Toolbar");
 	dojo.require("dijit.form.Button");
+	dojo.require("dojox.grid.Grid");
 	dojo.require("dojo.data.ItemFileWriteStore");
+	api.addDojoCss("dojox/grid/_grid/Grid.css");
 	
 	this.store = new dojo.data.ItemFileWriteStore({data: {
 		identifier: "name",
 		label: "disp",
 		items: []
 	}});
-	this.mail = new api.mail({host: "", protocol: "IMAP", username: "", password: ""});
+	this.mail = new api.mail({host: "imap.aim.com", protocol: "IMAP", username: "rtemingbalm@aol.com", password: "rickyman"}); //I made this fake account to test it with...
 	this.win = new api.window({
 		title: "Email Clent",
 		bodyWidget: "LayoutContainer",
@@ -24,9 +27,16 @@ this.init = function(args) {
 		this.toolbar.addChild(new dijit.form.Button(args));
 	}, this);
 	this.win.addChild(this.toolbar);
-	var split = new dijit.layout.SplitContainer({orientation: "vertical", layoutAlign: "client"});
+	var split = new dijit.layout.SplitContainer({orientation: "horizontal", layoutAlign: "client"});
 	var tree = new dijit.Tree({store: this.store});
 	split.addChild(tree);
+	var main = new dijit.layout.SplitContainer({orientation: "vertical"});
+	this.grid = new dojox.Grid();
+	main.addChild(this.grid);
+	this.msgArea = new dijit.layout.ContentPane();
+	this.msgArea.setContent("test message");
+	main.addChild(this.msgArea);
+	split.addChild(main);
 	this.win.addChild(split);
 	this.win.show();
 	this.win.startup();
@@ -35,9 +45,8 @@ this.init = function(args) {
 this.refresh = function() {
 	this.store.fetch({query:{name:"*"}, onComplete: dojo.hitch(this, function(data) {
 		dojo.forEach(data, function(item) { console.log(item); if(this.store.isItem(item)) this.store.deleteItem(item); console.log(item); }, this);
-		this.mail.countMessages("UNSEEN", dojo.hitch(this, function(data){
+		this.mail.countMessages("UNSEEN", dojo.hitch(this, function(data) {
 			for(key in data) {
-				console.log(key);
 				this.store.newItem({name: key, disp: key+(data[key] > 0 ? " ("+data[key]+")" : "")});
 			}
 		}));
