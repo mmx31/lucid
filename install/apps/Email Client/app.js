d45434e19152e3ec75fbf0b90f2e6415c4b0a0cb
Will Riley <will@psychdesigns.net>
2008-01-18 01:18:37 +0000

    Worked a bit more on the mail client
    git-svn-id: http://svn.psychdesktop.net/trunk@1021 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/apps/Email Client/app.js
+++ install/apps/Email Client/app.js
@@ -43,7 +43,7 @@ this.init = function(args) {
 	}, this);
 	this.win.addChild(this.toolbar);
 	var split = new dijit.layout.SplitContainer({orientation: "horizontal", layoutAlign: "client"});
-	var tree = new dijit.Tree({store: this.treeStore});
+	var tree = new dijit.Tree({store: this.treeStore, onClick: dojo.hitch(this, this.onTreeClick)});
 	split.addChild(tree);
 	var main = new dijit.layout.SplitContainer({orientation: "vertical"});
 	this.grid = new dojox.Grid({structure: [{
@@ -87,8 +87,36 @@ this.makeMailClasses = function() {
 		})
 	});
 }
+this.currentItem = null;
+this.onTreeClick = function(item) {
+	if(this.treeStore.getValue(this.currentItem, "name") != this.treeStore.getValue(item, "name")) {
+		this.currentItem = item;
+		this.updateUI();
+	}
+}
 this.updateUI=function() {
-	//TODO: here we would update the grid
+	if(this.currentItem && this.treeStore.getValue(this.currentItem, "root") == false) {
+		var folder = this.treeStore.getValue(this.currentItem, "name");
+		var account = this.treeStore.getValue(this.currentItem, "parentAccount");
+		var mailObj = false;
+		for(key in this.mail) {
+			if(this.prefs.getValue(this.mail[key].prefItem, "id") == account) {
+				mailObj = this.mail[key].down;
+				break;
+			}
+		}
+		if(mailObj) {
+			mailObj.listFolder({
+				folder: folder,
+				start: 0,
+				end: 4,
+				callback: dojo.hitch(this, function(data) {
+					var p = new dojox.grid.data.Objects(data);
+					this.grid.setModel(p);
+				})
+			});
+		}
+	}
 }
 this.folders = [];
 this.refresh = function() {
@@ -100,7 +128,9 @@ this.refresh = function() {
 				if(typeof items[0] == "undefined") {
 					var rootitem = this.treeStore.newItem({
 						name: "ROOT_"+this.prefs.getValue(mail.prefItem, "id"),
-						disp: this.prefs.getValue(mail.prefItem, "label")
+						disp: this.prefs.getValue(mail.prefItem, "label"),
+						root: true,
+						id: this.prefs.getValue(mail.prefItem, "id")
 					});
 				}
 				else var rootitem = false;
@@ -113,18 +143,21 @@ this._refreshHost = function(rootitem, mail) {
 	mail.down.countMessages("UNSEEN", dojo.hitch(this, function(f) {
 		for(key in f) {
 			if(!this.folders[key] || this.folders[key] < f[key]) {
-				this.updateUI();
 				for(k in f) {
-					this.treeStore.fetch({query: {name: k}, queryOptions: {deep: true}, scope: this, onComplete: function(item) {
+					this.treeStore.fetch({query: {name: k+"_"+this.treeStore.getValue(rootitem, "name")}, queryOptions: {deep: true}, scope: this, onComplete: function(item) {
 						var label = k+(f[k] > 0 ? " ("+f[k]+")" : "");
 						if(typeof item[0] == "undefined") {
-							this.treeStore.newItem({name: k, disp: label}, {parent: rootitem, attribute: "children"});
+							var newItem = this.treeStore.newItem({name: k+"_"+this.treeStore.getValue(rootitem, "name"), disp: label, parentAccount: this.treeStore.getValue(rootitem, "id"), root: false}, {parent: rootitem, attribute: "children"});
+							if(this.currentItem === null) {
+								this.currentItem = newItem;
+							}
 						}
 						else {
 							this.treeStore.setValue(item[0], "disp", label);
 						}
 					}});
 				}
+				this.updateUI();
 				break;
 			}
 		}
