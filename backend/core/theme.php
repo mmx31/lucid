54ec44ab7b469a6e60d2347079726fc310bd3344
Jay MacDonald <jaymac407@gmail.com>
2008-02-25 21:17:29 +0000

    backend returns JSON. slight issue with null's, possibly my XML programming
    git-svn-id: http://svn.psychdesktop.net/trunk@1207 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/core/theme.php
+++ backend/core/theme.php
@@ -16,21 +16,37 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 	*/
+error_reporting(0); //<<<<<<<<<<<<<<<<<<<LEAVE THIS ALONE!!!
+require("../lib/includes.php");
+require("../lib.xml.php");
 if($_GET['section'] == "get")
 {
 	if($_GET['action'] == "list")
 	{
 	    $dir = opendir("../../desktop/themes/");
+		$blah = new jsonOutput;
+		$p = array();
 		while(($file = readdir($dir)) !== false) {
 			if($file == '..' || $file == '.' || $file{0} == '.'){
 					continue;
 			} else {
 				$t = strtolower($file);
 				if(is_dir("../../desktop/themes/" . $file)){
-					echo($file."\n");
+					$xml = new Xml;
+					$in = $xml->parse('../../desktop/themes/'.$file.'/themeMeta.xml', 'FILE');
+					$p[] = array("name" => $in["name"], "author" => $in["author"], "email" => $in["email"], "version" => $in["version"], "wallpaper" => $in["wallpaper"], "preview" => $in["preview"]);
+					/*
+					$blah->append("name", $in["name"]);
+					$blah->append("author", $in["author"]);
+					$blah->append("email", $in["email"]);
+					$blah->append("version", $in["version"]);
+					$blah->append("wallpaper", $in["wallpaper"]);
+					$blah->append("preview", $in["preview"]);
+					*/
 				}
 			}
 		}
+		$blah->set($p);
 	}
 }
 ?>
