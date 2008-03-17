4ea4643b6f3ee6ed884ed38a93aa06bed79a86aa
Will Riley <will@psychdesigns.net>
2008-03-17 03:37:51 +0000

    bugfix again
    git-svn-id: http://svn.psychdesktop.net/trunk@1321 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/api/flash/flash.js
+++ desktop/api/flash/flash.js
@@ -3,9 +3,7 @@ if (dojox.flash.info.capable == true) {
 	dojox.flash.addLoadedListener(function() {
 		console.log("js:flash loaded");
 	});
-	dojox.flash.Embed.prototype = {
-		width: 1,
-		height: 1
-	};
+	dojox.flash.Embed.prototype.width = 1;
+	dojox.flash.Embed.prototype.height = 1;
 	dojox.flash.setSwf("./api/flash/objManager.swf", false);
 }
