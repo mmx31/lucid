a58021c9415723e93b4fd162e0bb94d9f2f18512
Will Riley <will@psychdesigns.net>
2008-03-24 01:40:39 +0000

    added a TODO:
    git-svn-id: http://svn.psychdesktop.net/trunk@1408 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/desktop/config.js
+++ desktop/desktop/config.js
@@ -108,6 +108,9 @@ desktop.config = {
 	 * 
 	 * Position the toaster popup will appear
 	 * Can be one of: ["br-up", "br-left", "bl-up", "bl-right", "tr-down", "tr-left", "tl-down", "tl-right"]
+	 * 
+	 * TODO:
+	 * 		this needs a configuration tool
 	 */
 	toasterPos: "tr-down",
 	/*
