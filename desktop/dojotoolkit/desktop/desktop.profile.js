4229835c6f4d2165312fa5adf97f8d70ec39c2bc
Will Riley <will@psychdesigns.net>
2008-03-27 19:00:09 +0000

    added a build profile
    git-svn-id: http://svn.psychdesktop.net/trunk@1466 815afad1-63b8-44ff-8d19-411ecde00fa5

---
+++ desktop/dojotoolkit/desktop/desktop.profile.js
@@ -0,0 +1,13 @@
+dependencies ={
+    layers:  [
+        {
+        name: "../desktop/desktop.js",
+        dependencies: ["desktop.desktop"]
+        }
+    ],
+    prefixes: [
+        [ "desktop", "../desktop" ],
+        [ "api", "../api" ],
+        [ "login", "../login" ]
+    ]
+};
