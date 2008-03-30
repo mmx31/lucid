24501dc68823a70402ce2683dfbe11cc68366cb7
Will Riley <will@psychdesigns.net>
2008-03-30 01:34:40 +0000

    redid the calculator. In the process of working on a math parser and something that does not rely on floating points to calculate a value (because that's risky buisness in JS)
    git-svn-id: http://svn.psychdesktop.net/trunk@1513 815afad1-63b8-44ff-8d19-411ecde00fa5

--- install/apps/Calculator/app.js
+++ install/apps/Calculator/app.js
@@ -1,54 +1,160 @@
 this.kill = function() {
-	if(!this.window.closed) { this.window.close(); }
+	if(!this.win.closed) this.win.close();
 }
-this.init = function(args)
-{
+
+this.init = function() {
+	dojo.require("dijit.form.Button");
+	dojo.require("dijit.form.TextBox");
 	dojo.require("dijit.layout.ContentPane");
-winHTML  = '<form style="height: 12%; overflow: hidden;" onSubmit="return desktop.app.instances['+this.instance+'].evaluate();"><input type="text" id="results'+this.instance+'" style="text-align: right; width: 100%;" /></form>';
-//------------------
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'(\'">(</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \')\'">)</button>';
-winHTML += '<button style="width: 25%; height: 16%; visibility: hidden;"> </button>'
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value = \'\'">C</button>';
-//------------------
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'7\'">7</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'8\'">8</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'9\'">9</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'/\'">/</button>';
-//------------------
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'4\'">4</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'5\'">5</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'6\'">6</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'*\'">*</button>';
-//------------------
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'1\'">1</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'2\'">2</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'3\'">3</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'-\'">-</button>';
-//------------------
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'0\'">0</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'.\'">.</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="desktop.app.instances['+this.instance+'].evaluate()">=</button>';
-winHTML += '<button style="width: 25%; height: 16%;" onClick="document.getElementById(\'results'+this.instance+'\').value += \'+\'">+</button>';
-//-------------------
-this.window = new api.Window({
-	title: "Calculator",
-	width: "200px",
-	height: "235px"
-});
-var b = new dijit.layout.ContentPane({layoutAlign: "client"});
-b.setContent(winHTML);
-this.window.addChild(b);
-this.window.show();
-this.window.startup();
-dojo.connect(this.window, "onClose", this, this.kill);
+	var win = this.win = new api.Window({
+		title: "Calculator",
+		width: "200px",
+		height: "270px",
+		onClose: dojo.hitch(this, "kill")
+	});
+	var textbox = new dijit.form.TextBox({
+		layoutAlign: "top",
+		style: "text-align: right;"
+	});
+	win.addChild(textbox);
+	var client = new dijit.layout.ContentPane({
+		layoutAlign: "client"
+	});
+	win.addChild(client);
+	var c = document.createElement("table");
+	dojo.style(c, {
+		border: "0px",
+		width: "100%",
+		height: "100%"
+	});
+	dojo.forEach([
+		["(", ")",  "", "C"],
+		["7", "8", "9", "/"],
+		["4", "5", "6", "*"],
+		["1", "2", "3", "-"],
+		["0", ".", "=", "+"]
+	], function(row) {
+		var rowNode = document.createElement("tr");
+		dojo.forEach(row, function(cell) {
+			var cellNode = document.createElement("td");
+			dojo.style(cellNode, "padding", "0px");
+			if(cell != "") {
+				var button = new dijit.form.Button({
+					style: "width: 100%; height: 100%; margin: 0px;",
+					label: cell,
+					onClick: dojo.hitch(this, function(){
+						if(parseInt(cell)+"" != "NaN" && this.answerShown) this.clear(textbox); 
+						if(cell == "=") return this.onSubmit(textbox);
+						if(cell == "C") return this.clear(textbox);
+						this.answerShown = false;
+						textbox.setValue(textbox.getValue() + cell);
+					})
+				});
+				dojo.style(button.focusNode, {
+					width: "100%",
+					height: "100%"
+				});
+				cellNode.appendChild(button.domNode);
+			}
+			else {
+				this.eNode = document.createElement("div");
+				dojo.style(this.eNode, {
+					textAlign: "center",
+					color: "red",
+					fontWeight: "bold"
+				});
+				cellNode.appendChild(this.eNode);
+			}
+			rowNode.appendChild(cellNode);
+		}, this);
+		c.appendChild(rowNode);
+	}, this);
+	client.setContent(c);
+	win.show();
 }
-this.evaluate = function()
-{
-     if(document.getElementById("results"+this.instance).value != "")
-     {
-          try{ eval("document.getElementById('results'+this.instance).value = "+document.getElementById("results"+this.instance).value); }
-          catch(e) { document.getElementById('results'+this.instance).value = "ERROR"; }
-     }
-     return false;
+this.clear = function(t) {
+	this.answerShown = false;
+	this.eNode.textContent = "";
+	t.setValue("");
+}
+this.answerShown = false;
+this.onSubmit = function(t) {
+	this.answerShown = true;
+	var v = t.getValue();
+	this.eNode.textContent = "";
+	if(!this.validate(v)) return this.eNode.textContent = "E";
+	t.setValue(eval(t.getValue()));
+}
+
+this.validate = function(v) {
+	//Check for matching parenthesis
+	if(v.split("(").length != v.split(")").length) return false;
+	//Check for invalid characters
+	for(var i=0; i < v.length; i++) {
+		var c = v[i];
+		if(!(parseInt(c)+"" != "NaN"
+		|| c == ")"
+		|| c == "("
+		|| c == "/"
+		|| c == "*"
+		|| c == "+"
+		|| c == "-"
+		|| c == "."
+		)) return false;
+	}
+	//Check for stray decimal points
+	var parts = v.split(".");
+	for(i=0; i<parts.length;i++) {
+		if(parts[i] == "" && i!=0) return false;
+		if(parseInt(parts[i].charAt(0))+"" == "NaN" && i != 0) return false;
+	}
+	return true;
+}
+
+this.getValue = function(op) {
+	if(op.indexOf("+") != -1) return this.sum.apply(this, op.split("+"));
+	if(op.indexOf("-") != -1) return this.difference.apply(this, op.split("-"));
+	if(op.indexOf("*") != -1) return this.product.apply(this, op.split("*"));
+	if(op.indexOf("/") != -1) return this.quotient.apply(this, op.split("/"));
+}
+
+this.offsetDecimal = function(a, b) {
+	if((a.indexOf(".") == -1 && b.indexOf(".") == -1)
+	|| ((a.length - a.indexOf(".") == b.length - b.indexOf(".")) && (a.indexOf(".") != -1 && b.indexOf(".") != -1))) return [a, b];
+	if(a.indexOf(".") == -1) a += ".";
+	if(b.indexOf(".") == -1) b += ".";
+	var g = a.length - a.indexOf(".") > b.length - b.indexOf(".") ? a : b;
+	var l = a.length - a.indexOf(".") <= b.length - b.indexOf(".") ? a : b;
+	while(g.length - g.indexOf(".") > l.length - l.indexOf(".")) {
+		l += "0";
+	}
+	if(l.charAt(l.length-1) == ".") l = l.substring(0, l.length-1);
+	if(g.charAt(g.length-1) == ".") g = g.substring(0, g.length-1);
+	return a.length - a.indexOf(".") > b.length - b.indexOf(".") ? [g, l] : [l, g];
+}
+
+this.sum = function(a, b) {
+	var num = this.offsetDecimal(a+"", b+"");
+	a = num[0]; b=num[1];
+	var l = (a.length > b.length ? a.length : b.length)-1;
+	var carryFlag = 0;
+	var retVal = "";
+	var checkFlag = function(i) {
+		if(i >= 10) {
+			carryFlag = parseInt((i+"").substring(0, (i+"").length-2));
+			return (i+"").charAt((i+"").length-1);
+		}
+		return i;
+	}
+	for(i=l; i>=0; i--) {
+		if(a.charAt(i) == ".") retVal = "." + retVal;
+		if(a.charAt(i) == "") {
+			retVal = checkFlag(parseInt(b.charAt(i)) + carryFlag) + retVal;
+		}
+		else if(b.charAt(i) == "") {
+			retVal = checkFlag(parseInt(a.charAt(i)) + carryFlag) + retVal;
+		}
+		else retVal = checkFlag((parseInt(a.charAt(i)) + parseInt(b.charAt(i))) + carryFlag);
+	}
+	return retVal;
 }
