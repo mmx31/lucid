25101fd24472773083057582567426c07a07c943
Will Riley <will@psychdesigns.net>
2008-04-08 01:43:51 +0000

    added es translations for messages
    git-svn-id: http://svn.psychdesktop.net/trunk@1654 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/nls/es/messages.js
+++ desktop/dojotoolkit/desktop/nls/es/messages.js
@@ -1,3 +1,10 @@
 ({
-	"allreadyExists": "Un tema con ese nombre ya existe"
+	"allreadyExists": "Un tema con ese nombre ya existe",
+	"editingFile": "Edición el archivo de \"%s\"",
+	"noFileOpen": "No abra archivos",
+	"chooseFileOpen": "Elija un archivo para abrir",
+	"chooseFileSave": "Elija un archivo para guardar como",
+	"openingFile": "Apertura de archivos de \"%s\"...",
+	"editingFile": "Edición de archivo de \"%s\"",
+	"fileSaved": "Archivo grabado!"
 })
