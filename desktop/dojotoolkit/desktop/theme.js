9f98ec17d0fd27b41851dd4ee27c2d2ee4af85c1
Will Riley <will@psychdesigns.net>
2008-04-16 20:43:25 +0000

    brought desktop.theme documentation over
    git-svn-id: http://svn.psychdesktop.net/trunk@1736 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/theme.js
+++ desktop/dojotoolkit/desktop/theme.js
@@ -1,15 +1,9 @@
 dojo.provide("desktop.theme");
-/*
- * Class: desktop.theme
- * 
- * Takes care of themes
- */
 desktop.theme = {
-	/*
-	 * Property: fileList
-	 * 
-	 * The different CSS files to load for each theme
-	 */
+	//	summary:
+	//		Theme manager
+	//	fileList: Array
+	//		The different CSS files to load for each theme
 	fileList: ["theme", "window", "icons", "dijit"],
 	draw: function()
 	{
@@ -28,16 +22,12 @@ desktop.theme = {
 			desktop.theme.set(conf.theme);
 		});
 	},
-	/*
-	 * Method: set
-	 * 
-	 * Sets the theme
-	 * 
-	 * Arguments:
-	 * 		theme - the theme to use
-	 */
 	set: function(/*String*/theme)
 	{
+		//	summary:
+		//		Sets the theme
+		//	theme:
+		//		the theme to use
 		desktop.config.theme = theme;
 		dojo.forEach(this.fileList, function(e) {
 			dojo.byId("desktop_theme_"+e).href ="./themes/"+desktop.config.theme+"/"+e+".css";
@@ -52,8 +42,39 @@ desktop.theme = {
 	 * 		callback - a callback function
 	 * 		sync - should the call be syncronous? (defaults to false)
 	 */
+	/*=====
+	_listArgs: {
+		//	sysname: String
+		//		the system name of the string (it's directory's name in /desktop/themes/)
+		sysname: "green",
+		//	name: String
+		//		the displayable name of the theme
+		name:"Green",
+		//	author: String
+		//		the author of the theme
+		author:"Psychcf",
+		//	email: String
+		//		the email address of the author
+		email:"will@psychdesigns.net",
+		//	version: String
+		//		The version of the theme
+		version:"1.0",
+		//	wallpaper: String
+		//		the wallpaper for the theme (file located in the theme's dir)
+		wallpaper:"wallpaper.png",
+		//	preivew: String
+		//		a screenshot of the theme (file located in the theme's dir)
+		preview:"screenshot.png"
+	}
+	=====*/
 	list: function(/*Function*/callback, /*Boolean?*/sync)
 	{
+		//	summary:
+		//		Pases a list of the themes to the callback provided
+		//	callback:
+		//		a callback function. First arg is an array of desktop.theme._listArgs objects.
+		//	sync:
+		//		Should the call be synchronous? defaults to false.
 		api.xhr({
 			backend: "core.theme.get.list",
 			load: callback,
