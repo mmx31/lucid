a8c27db73c36ba5b69b86bbc564a33203f78c182
Will Riley <will@psychdesigns.net>
2008-05-20 20:19:57 +0000

    minor tweak to is_writable check
    git-svn-id: http://svn.psychdesktop.net/trunk@1897 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/lib/util.php
+++ backend/lib/util.php
@@ -27,36 +27,6 @@ if (get_magic_quotes_gpc())
 		$_GET[$key] = stripslashes($value);
 	}
 }
-
-//sessions and cookies
-function get_basepath() {
-	$curpath = explode("/", $_SERVER['REQUEST_URI']);
-	$dir = $GLOBALS['installing'] ? "install" : ($GLOBALS['mobile'] ? "mobile" : "backend");
-	while($curpath[count($curpath)-1] != $dir) {
-		if(count($curpath) == 0) return "/";
-		array_pop($curpath);
-	}
-	array_pop($curpath);
-	return implode("/", $curpath) . "/";
-}
-$sesPath = $GLOBALS['path']."/../tmp/sessions/";
-if(!is_dir($sesPath)) mkdir($sesPath);
-session_save_path($sesPath);
-if(!is_writable($sesPath)) die("sessions path is not writable. (/tmp/session)");
-
-$time = 60*60*24*365;
-session_name('desktop_session');
-session_set_cookie_params($time, get_basepath());
-ini_set("session.gc_maxlifetime",$time);
-session_start();
-
-
-//for debugging
-function desktop_errorHandler($exception) {
-	internal_error("generic_err", $exception->getMessage());
-}
-set_exception_handler("desktop_errorHandler");
-
 //util functions
 function internal_error($type, $msg="")
 {
@@ -74,3 +44,32 @@ function import($module) {
 	$file = $GLOBALS['path'] . $path . ".php";
 	return @include_once($file);
 }
+//sessions and cookies
+function get_basepath() {
+	$curpath = explode("/", $_SERVER['REQUEST_URI']);
+	$dir = $GLOBALS['installing'] ? "install" : ($GLOBALS['mobile'] ? "mobile" : "backend");
+	while($curpath[count($curpath)-1] != $dir) {
+		if(count($curpath) == 0) return "/";
+		array_pop($curpath);
+	}
+	array_pop($curpath);
+	return implode("/", $curpath) . "/";
+}
+$sesPath = $GLOBALS['path']."/../tmp/sessions/";
+if(!is_dir($sesPath)) mkdir($sesPath);
+session_save_path($sesPath);
+if(!is_writable($sesPath)) internal_error("generic_err", "sessions path is not writable. (/tmp/session)");
+
+$time = 60*60*24*365;
+session_name('desktop_session');
+session_set_cookie_params($time, get_basepath());
+ini_set("session.gc_maxlifetime",$time);
+session_start();
+
+
+//for debugging
+function desktop_errorHandler($exception) {
+	internal_error("generic_err", $exception->getMessage());
+}
+set_exception_handler("desktop_errorHandler");
+
