895ce2fc3d924a8808f77bdb7967cf99f4db9788
Will Riley <will@psychdesigns.net>
2008-05-31 21:59:27 +0000

    *Merged installer changes from 1.0 branch to trunk *Modified the build profile to create layer files for each app
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@1950 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/util/buildscripts/profiles/desktop.profile.js
+++ desktop/dojotoolkit/util/buildscripts/profiles/desktop.profile.js
@@ -1,5 +1,6 @@
 dependencies ={
     layers:  [
+		//CORE
         {
         	name: "../desktop/desktop.js",
         	dependencies: ["desktop.desktop"]
@@ -7,7 +8,76 @@ dependencies ={
         {
         	name: "../login/Form.js",
         	dependencies: ["login.Form"]
-        }
+        },
+		//APPS
+		{
+			name: "../desktop/apps/AccountInfo.js",
+			dependencies: ["desktop.apps.AccountInfo"]
+		},
+		{
+			name: "../desktop/apps/AdminPanel.js",
+			dependencies: ["desktop.apps.AdminPanel"]
+		},
+		{
+			name: "../desktop/apps/AppearanceConfig.js",
+			dependencies: ["desktop.apps.AppearanceConfig"]
+		},
+		{
+			name: "../desktop/apps/Calculator.js",
+			dependencies: ["desktop.apps.Calculator"]
+		},
+		{
+			name: "../desktop/apps/Checkers.js",
+			dependencies: ["desktop.apps.Checkers"]
+		},
+		{
+			name: "../desktop/apps/FeedReader.js",
+			dependencies: ["desktop.apps.FeedReader"]
+		},
+		{
+			name: "../desktop/apps/FileBrowser.js",
+			dependencies: ["desktop.apps.FileBrowser"]
+		},
+		{
+			name: "../desktop/apps/ImageViewer.js",
+			dependencies: ["desktop.apps.ImageViewer"]
+		},
+		{
+			name: "../desktop/apps/KatanaIDE.js",
+			dependencies: ["desktop.apps.KatanaIDE"]
+		},
+		{
+			name: "../desktop/apps/MineSweep.js",
+			dependencies: ["desktop.apps.MineSweep"]
+		},
+		{
+			name: "../desktop/apps/MusicPlayer.js",
+			dependencies: ["desktop.apps.MusicPlayer"]
+		},
+		{
+			name: "../desktop/apps/StartupConfig.js",
+			dependencies: ["desktop.apps.StartupConfig"]
+		},
+		{
+			name: "../desktop/apps/TaskManager.js",
+			dependencies: ["desktop.apps.TaskManager"]
+		},
+		{
+			name: "../desktop/apps/Terminal.js",
+			dependencies: ["desktop.apps.Terminal"]
+		},
+		{
+			name: "../desktop/apps/TextEditor.js",
+			dependencies: ["desktop.apps.TextEditor"]
+		},
+		{
+			name: "../desktop/apps/WebBrowser.js",
+			dependencies: ["desktop.apps.WebBrowser"]
+		},
+		{
+			name: "../desktop/apps/WordProcessor.js",
+			dependencies: ["desktop.apps.WordProcessor"]
+		}
     ],
     prefixes: [
         [ "desktop", "../desktop" ],
