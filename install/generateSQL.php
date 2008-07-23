2291a2d9caeecda1ec7f9b8e9712ab46736e7879
Will Riley <will@psychdesigns.net>
2008-07-23 18:18:12 +0000

    Started work on the sql table creation stuff. I still have to finish it, but I made a good dent in it.
    git-svn-id: http://svn.psychdesktop.net/trunk@1989 815afad1-63b8-44ff-8d19-411ecde00fa5

---
+++ install/generateSQL.php
@@ -0,0 +1,40 @@
+<?php
+    class sqlSchema {
+		var $_db;
+		public function __construct($schemaFile, $db) {
+			//bootstrap
+			self::$_db = $db;
+			$fileContents = file_get_contents($schemaFile);
+			$schema = Zend_Json::decode($fileContents);
+			
+			//get the metadata, such as charset
+			$charset = $schema['__META__']['charset'];
+			unset($schema['__META__']);
+			
+			//make each table
+			foreach($schema as $table => $fields) {
+				self::makeTable($table, $fields, $charset);
+			}
+		}
+		private function makeTable($name, $fields, $charset) {
+			$sql = "CREATE TABLE ".self::$_db->quoteIdentifier($name)." (";
+			foreach($fields as $name => $properties) {
+				$sql .= self::makeField($name,
+										$properties["type"],
+										$properties["length"],
+										$properties["default"],
+										$properties["null"],
+										$properties["key"],
+										$properties["primarykey"]);
+			}
+			$sql .= ")";
+			//TODO: set the charset/collate
+		}
+		private function makeField($name, $type, $length, $default=null, $null=false, $key=false, $pk=false) {
+			//TODO: change the type depending on the database backend used
+			$type = strtoupper($type);
+			return self::$_db->quoteIdentifier($name)." ".$type."(".$length.")"
+					. ($null ? " NULL" : " NOT NULL") . ($default ? " DEFAULT ".self::$_db->quote($default) : "")
+					. ($key && !$pk ? " UNIQUE KEY" : "") . ($pk && !$key ? " PRIMARY KEY" : "");
+		}
+    }
