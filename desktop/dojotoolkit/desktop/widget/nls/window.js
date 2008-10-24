67120653e0232ec2115c35ef75a843d3f176281f
Will Riley <will@psychdesigns.net>
2008-10-24 22:35:37 +0000

    forgot to add translations for previous commit
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2186 815afad1-63b8-44ff-8d19-411ecde00fa5

---
+++ desktop/dojotoolkit/desktop/widget/nls/window.js
@@ -0,0 +1,8 @@
+({
+    "close": "Close",
+    "minimize": "Minimize",
+    "maximize": "Maximize",
+    "unmaximize": "Unmaximize",
+    "restore": "Restore",
+    "alwaysOnTop": "Always On Top"
+})
