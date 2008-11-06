19175b88912294a0cc844e37505960ffafc04d35
Will Riley <will@psychdesigns.net>
2008-11-06 00:22:18 +0000

    removed readme, anything explained there is now explained in the installation guide
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2247 815afad1-63b8-44ff-8d19-411ecde00fa5

--- backend/configuration.php
+++ backend/configuration.php
@@ -1,10 +1,10 @@
 <?php
 	$GLOBALS['db'] = Array(
-		"database" => "sqlite:////var/db/desktop.db?mode=666",
+		"database" => "mysqli://root:toor@localhost/desktop",
 		"prefix" => ""
 	);
 	$GLOBALS['conf'] = Array(
-		"salt" => "c125da007063e542d39ee01b2916ee0b",
+		"salt" => "cdefd39f1dd302ac2521034e5f9c4c93",
 		"public" => true,
 		"crosstalkThrottle" => false
 	);
