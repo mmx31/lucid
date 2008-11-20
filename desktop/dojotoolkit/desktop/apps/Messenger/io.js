5fffa292e0001b99dd8e61be8b585a2d12bc82d3
Will Riley <will@psychdesigns.net>
2008-11-20 22:39:05 +0000

    got the IM client 100% working now
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2287 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/apps/Messenger/io.js
+++ desktop/dojotoolkit/desktop/apps/Messenger/io.js
@@ -2,18 +2,26 @@ dojo.provide("desktop.apps.Messenger.io");
 
 dojo.extend(desktop.apps.Messenger, {
     listener: null,
-    setListener: function() {
+    currentUsername: null,
+    setListener: function(){
         this.listener = desktop.crosstalk.subscribe("IM", dojo.hitch(this, "recieveMessage"), this.instance); 
     },
-    removeListener: function() {
+    removeListener: function(){
         desktop.crosstalk.unsubscribe(this.listener);
     },
-    sendMessage: function(uid, msg) {
+    sendMessage: function(uid, msg){
         this.pushMsg(uid, msg, true);
         desktop.crosstalk.publish("IM", {text: msg}, uid, this.sysname);
     },
-    recieveMessage: function(msg) {
+    recieveMessage: function(msg){
         var uid = msg._crosstalk.sender;        
         this.pushMsg(uid, msg.text, false);
+    },
+    getCurrentUsername: function(cback){
+        if(this.currentUsername) cback(this.currentUsername);
+        desktop.user.get({callback: dojo.hitch(this, function(data) {
+            this.currentUsername = data.username;
+            cback(this.currentUsername);
+        })});
     }
 });
