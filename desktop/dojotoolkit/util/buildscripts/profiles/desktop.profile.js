5fda14ef9a03b265179768f5ebbc0719152e5c1f
Will Riley <will@psychdesigns.net>
2008-11-24 21:00:16 +0000

    added some optimizations to the apps for the build profile
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2321 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/util/buildscripts/profiles/desktop.profile.js
+++ desktop/dojotoolkit/util/buildscripts/profiles/desktop.profile.js
@@ -12,35 +12,43 @@ dependencies ={
 		//APPS
 		{
 			name: "../desktop/apps/AccountInfo.js",
-			dependencies: ["desktop.apps.AccountInfo"]
+			dependencies: ["desktop.apps.AccountInfo"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/AdminPanel.js",
-			dependencies: ["desktop.apps.AdminPanel"]
+			dependencies: ["desktop.apps.AdminPanel"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/AppearanceConfig.js",
-			dependencies: ["desktop.apps.AppearanceConfig"]
+			dependencies: ["desktop.apps.AppearanceConfig"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/Calculator.js",
-			dependencies: ["desktop.apps.Calculator"]
+			dependencies: ["desktop.apps.Calculator"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/Checkers.js",
-			dependencies: ["desktop.apps.Checkers"]
+			dependencies: ["desktop.apps.Checkers"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/Contacts.js",
-			dependencies: ["desktop.apps.Contacts"]
+			dependencies: ["desktop.apps.Contacts"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/FeedReader.js",
-			dependencies: ["desktop.apps.FeedReader"]
+			dependencies: ["desktop.apps.FeedReader"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/FileBrowser.js",
-			dependencies: ["desktop.apps.FileBrowser"]
+			dependencies: ["desktop.apps.FileBrowser"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/ImageViewer.js",
@@ -48,47 +56,58 @@ dependencies ={
 		},
 		{
 			name: "../desktop/apps/KatanaIDE.js",
-			dependencies: ["desktop.apps.KatanaIDE"]
+			dependencies: ["desktop.apps.KatanaIDE"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/Messenger.js",
-			dependencies: ["desktop.apps.Messenger"]
+			dependencies: ["desktop.apps.Messenger"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/MineSweep.js",
-			dependencies: ["desktop.apps.MineSweep"]
+			dependencies: ["desktop.apps.MineSweep"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/MusicPlayer.js",
-			dependencies: ["desktop.apps.MusicPlayer"]
+			dependencies: ["desktop.apps.MusicPlayer"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/StartupConfig.js",
-			dependencies: ["desktop.apps.StartupConfig"]
+			dependencies: ["desktop.apps.StartupConfig"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/TaskManager.js",
-			dependencies: ["desktop.apps.TaskManager"]
+			dependencies: ["desktop.apps.TaskManager"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/Terminal.js",
-			dependencies: ["desktop.apps.Terminal"]
+			dependencies: ["desktop.apps.Terminal"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/TextEditor.js",
-			dependencies: ["desktop.apps.TextEditor"]
+			dependencies: ["desktop.apps.TextEditor"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/UpdateManager.js",
-			dependencies: ["desktop.apps.UpdateManager"]
+			dependencies: ["desktop.apps.UpdateManager"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/WebBrowser.js",
-			dependencies: ["desktop.apps.WebBrowser"]
+			dependencies: ["desktop.apps.WebBrowser"],
+            layerDependencies: ["../desktop/desktop.js"]
 		},
 		{
 			name: "../desktop/apps/WordProcessor.js",
-			dependencies: ["desktop.apps.WordProcessor"]
+			dependencies: ["desktop.apps.WordProcessor"],
+            layerDependencies: ["../desktop/desktop.js"]
 		}
     ],
     prefixes: [
