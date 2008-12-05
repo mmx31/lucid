2ae6a3c54be6228aa595230a2956ce41ca415a23
Will Riley <will@psychdesigns.net>
2008-12-05 20:40:18 +0000

    Added a timestamp to the save message in the statusbar of the word processor app
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2392 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/apps/WordProcessor.js
+++ desktop/dojotoolkit/desktop/apps/WordProcessor.js
@@ -6,6 +6,7 @@ dojo.require("dijit._editor.plugins.TextColor");
 dojo.require("dijit._editor.plugins.FontChoice");
 dojo.require("dijit._editor.plugins.LinkDialog");
 dojo.require("dijit._editor.plugins.TabIndent");
+dojo.require("dojo.date");
 
 dojo.requireLocalization("desktop", "common");
 dojo.requireLocalization("desktop", "apps");
@@ -163,8 +164,9 @@ dojo.declare("desktop.apps.WordProcessor", desktop.apps._App, {
 	processSave: function(){
 		var msg = dojo.i18n.getLocalization("desktop", "messages");
 	    if (this.editing){
-        desktop.filesystem.writeFileContents(this.fileEditing, "<html>"+this.editor.getValue()+"</html>");
-        this.statusbar.attr("label", msg.fileSaved);
+            desktop.filesystem.writeFileContents(this.fileEditing, "<html>"+this.editor.getValue()+"</html>");
+            var p = dojo.date.locale.format(new Date());
+            this.statusbar.attr("label", msg.fileSaved+" ("+p+")");
 	    }
 	    else {
 	        this.processSaveAs();
