4d22a7514e1fe6158432649e60c0e29f5a2c30f7
Will Riley <will@psychdesigns.net>
2008-12-09 21:08:20 +0000

    Fixes #282
    git-svn-id: http://svn.psychdesktop.net/trunk@2398 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/dojo/_base/_loader/bootstrap.js
+++ desktop/dojotoolkit/dojo/_base/_loader/bootstrap.js
@@ -185,7 +185,7 @@ dojo.global = {
 =====*/
 	dojo.locale = d.config.locale;
 	
-	var rev = "$Rev: 15729 $".match(/\d+/);
+	var rev = "$Rev: 15997 $".match(/\d+/);
 
 	dojo.version = {
 		// summary: 
@@ -200,7 +200,7 @@ dojo.global = {
 		//		Descriptor flag. If total version is "1.2.0beta1", will be "beta1"
 		//	revision: Number
 		//		The SVN rev from which dojo was pulled
-		major: 1, minor: 2, patch: 2, flag: "",
+		major: 1, minor: 2, patch: 3, flag: "",
 		revision: rev ? +rev[0] : 999999, //FIXME: use NaN?
 		toString: function(){
 			with(d.version){
