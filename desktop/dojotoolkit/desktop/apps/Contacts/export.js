889e5e41e3b56cd450dfed45f5db1b45ac210b1e
Will Riley <will@psychdesigns.net>
2009-01-05 21:20:55 +0000

    more work on #209
    git-svn-id: http://svn.psychdesktop.net/branches/1.0@2430 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/apps/Contacts/export.js
+++ desktop/dojotoolkit/desktop/apps/Contacts/export.js
@@ -24,16 +24,53 @@ dojo.provide("desktop.apps.Contacts.export");
             store.fetch({
                 query: {id: "*"},
                 onItem: function(item){
-                   var card = "BEGIN:VCARD\nVERSION:3.0\n";
+                   var card = "BEGIN:VCARD\r\nVERSION:3.0\r\n";
                    for(var key in keys){
-                       if(store.hasAttribute(item, key) && store.getValue(item, key) != "")
-                           card += keys[key]+":"+store.getValue(item, key)+"\n";
+                       if(key == "address"){
+                            var types = ["work", "home"];
+                            for(var t in types){
+                                var type = types[t];
+                                //grab all the address fields, merge into one
+                                var fields = ["pobox", "address", "city", "state", "zip", "country"];
+                                var parts = [];
+                                var labels = [];
+                                for(var i in fields){
+                                    var field = fields[i]+"-"+type;
+                                    if(store.hasAttribute(item, field)){
+                                        labels.push(type+" "+fields[i]);
+                                        parts.push(store.getValue(item, field));
+                                    }
+                                }
+                                card += "LABEL;TYPE="+type+":"+labels.join("\\n")+"\r\n";
+                                card += keys[key]+";TYPE="+type.toUpperCase()+":";
+                                card += parts.join(";");
+                                card += "\r\n";
+                            }
+                       }
+                       else if(key == "phone"){
+                            var types=["work", "home", "mobile", "fax"];
+                            for(var t in types){
+                                var type = types[t];
+                                var field = "phone-"+type;
+                                var vcardFields = {
+                                    work: "WORK,VOICE",
+                                    home: "HOME,VOICE",
+                                    mobile: "MOBILE,VOICE",
+                                    fax: "WORK,FAX"
+                                }
+                                if(store.hasAttribute(item, field)){
+                                    card += "TEL;TYPE="+vcardFields[type]+":"+store.getValue(item, field);
+                                }
+                            }
+                       }
+                       else if(store.hasAttribute(item, key) && store.getValue(item, key) != "")
+                            card += keys[key]+":"+store.getValue(item, key)+"\r\n";
                    }
                    card += "END:VCARD";
                    data.push(card);
                 },
                 onComplete: function(){
-                   desktop.filesystem.writeFileContents(path, data.join("\n\n"), onComplete, onError);
+                   desktop.filesystem.writeFileContents(path, data.join("\r\n\r\n"), onComplete, onError);
                 }
             });
         }
