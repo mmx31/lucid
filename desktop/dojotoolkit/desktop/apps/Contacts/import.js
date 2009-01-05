b8f7f0efa5ae140eb01b24b3017fc991cbd3cf6a
Will Riley <will@psychdesigns.net>
2009-01-05 21:20:55 +0000

    more work on #209
    git-svn-id: http://svn.psychdesktop.net/trunk@2430 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktop/dojotoolkit/desktop/apps/Contacts/import.js
+++ desktop/dojotoolkit/desktop/apps/Contacts/import.js
@@ -20,6 +20,26 @@ dojo.provide("desktop.apps.Contacts.import");
         },
         importData: function(path, onComplete, onError){
             var store = this.contactStore;
+            var parseType = function(params){
+                var types = {
+                    FAX: "fax",
+                    HOME: "home",
+                    WORK: "work",
+                    MOBILE: "mobile"
+                }
+                var p = params.split(";");
+                for(var i in p){
+                    if(p[i] == "" || p[i].indexOf("=") === -1) continue;
+                    var kv = p[i].split("=");
+                    if(kv[0] != "TYPE") continue;
+                    for(var t in types){
+                        if(kv[1].indexOf(t) !== -1){
+                            return types[t];
+                        }
+                    }
+                    return "";
+                }
+            }
             desktop.filesystem.readFileContents(path, function(data){ 
                 var lines = (data+"\r\n").split("\n");
                 var vcard = {};
@@ -36,12 +56,43 @@ dojo.provide("desktop.apps.Contacts.import");
                         return;
                     }
                     var key = info[1];
+                    var params = info[2];
                     var value = info[3];
                     if(key == "BEGIN"){
                         vcard = {id: (new Date()).getTime().toString()+(counter++)}
                     }
                     else if(key == "END"){
                         vcards.push(vcard);
+                    }
+                    else if(keys[key] == "address"){
+                        //import address
+                        var type = "";
+                        if(!params){
+                            // just throw it in home I guess...
+                            type = "home";
+                        }else{
+                            type = parseType(params);
+                        }
+                        if(type != ""){
+                            // ok, now break out each part of the address
+                            var parts = value.split(";");
+                            var addrKeys = ["pobox", "address", "city", "state", "zip", "country"];
+                            for(var i in parts){
+                                store.setValue(item, addrKeys[i]+"-"+type, parts[i]);
+                            }
+                        }
+                    }
+                    else if(keys[key] == "phone"){
+                        //import phone
+                        var type="";
+                        if(!params){
+                            type = "home";
+                        }else{
+                            type = parseType(params);
+                        }
+                        if(type != ""){
+                            store.setValue(item, "phone-"+type, value);
+                        }
                     }
                     else if(keys[key]){
                         vcard[keys[key]] = value;
