3683a260b6f55c3dece93e3c98b8145461a92554
Will Riley <will@psychdesigns.net>
2009-01-15 16:06:27 -0500

    Put a button on the user applet, that when pressed launches AccountInfo

--- desktop/dojotoolkit/desktop/ui/applets/User.js
+++ desktop/dojotoolkit/desktop/ui/applets/User.js
@@ -1,12 +1,18 @@
 dojo.provide("desktop.ui.applets.User");
+dojo.require("dijit.form.Button");
 
 dojo.declare("desktop.ui.applets.User", desktop.ui.Applet, {
     dispName: "User",
     postCreate: function(){
-        var labelNode = this.containerNode;
+        var button = new dijit.form.Button({
+            label: " ",
+            onClick: dojo.hitch(desktop.apps, "launch", "AccountInfo")
+        });
         desktop.user.get({callback: function(data){
-            labelNode.innerHTML = data.name || data.username;
+            console.log(data);
+            button.attr("label", data.name || data.username);
         }});
+        this.containerNode.appendChild(button.domNode);
         this.inherited(arguments);
     }
 });
